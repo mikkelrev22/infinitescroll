3b362343f529de16afcee2b24243506d88b941ca
Michael Thompson <michael.220488@gmail.com>
2020-11-23 09:01:39 -0800

    webpack

---
+++ client/src/components/app.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import axios from 'axios'
+
+class App extends React.Component {
+  constructor () {
+    super ()
+    this.state = {
+    }
+  }
+  render () {
+    return (
+      <div>
+        hello, world
+      </div>
+    )
+  }
+}
+export default App
---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path')
+module.exports = {
+  entry: `./client/src/index.js`,
+  output: {
+    path: path.resolve(__dirname, './client/public'),
+    filename: "bundle.js",
+    publicPath: '/'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js|jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',          
+        options:{
+          presets: [
+            ["@babel/preset-env", {
+            "targets": "defaults" 
+            }],  '@babel/preset-react'
+          ]
+        }
+      }
+    ]
+  }
+};
